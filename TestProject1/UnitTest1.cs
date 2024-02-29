eac78f10b6abae7619ca006e2de970fd34314302
Vignesh <rayankivignesh@gmail.com>
2024-02-29 11:14:50 +0530

    Added unit-Test function

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,3 +1,6 @@
+using System.Numerics;
+using WebApplication1.Modules;
+
 namespace TestProject1
 {
     public class Tests
@@ -8,7 +11,17 @@ namespace TestProject1
             int i = 1;
             bool result = false;
             if (i == 1) result = true;
-            Assert.True(result, "valus hould be equal to 1");
+            Assert.True(result, "valus should be equal to 1");
+        }
+
+        [Test]
+        public void CheckAddFunction()
+        {
+            Functions fn= new Functions();
+            int y = fn.Add(5, 5);
+            bool result = false;
+            if(y== 10) result = true;
+            Assert.True(result, "value should be equa; to 10");
         }
     }
 }
